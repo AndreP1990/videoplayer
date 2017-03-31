42e6d4627d1290184a9149e88a2d836ad5549221
AndreP1990 <diefeuerameise@gmail.com>
2017-03-31 09:48:42 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function PlayPause(btn, vid){
+    
+    var vid = document.getElementById(vid);
+    if(vid.paused){
+        
+        vid.play();
+        btn.innerHTML = "Pause";
+        
+    } else {
+        
+        vid.pause();
+        btn.innerHTML = "Play";
+        
+    }
+}
