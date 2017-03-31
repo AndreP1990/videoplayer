256f6dfd93da4691277ea3be55236e4b2a9b5d77
AndreP1990 <diefeuerameise@gmail.com>
2017-03-31 12:52:22 +0200

    controlbar style, create videobox

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-function PlayPause(btn, vid){
+function PlayPause(btn, video){
     
-    var vid = document.getElementById(vid);
+    var vid = document.getElementById(video);
     if(vid.paused){
         
         vid.play();
-        btn.innerHTML = "Pause";
+        btn.innerHTML = "<img src = 'img/pause.svg'>";
         
     } else {
         
         vid.pause();
-        btn.innerHTML = "Play";
+        btn.innerHTML = "<img src = 'img/play.svg'>";
         
     }
 }
